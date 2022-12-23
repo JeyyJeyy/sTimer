55140025b14c7cb97e9a23b58794693aaf6c8d8b
JeyJey <44325723+JeyyJeyy@users.noreply.github.com>
2022-12-23 11:29:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const inp = readline.createInterface({
 });
 var Timer = require('timer-machine')
 var myTimer = new Timer()
-let countdownn = 25200000;
+let countdownn = 25200;
+let mod = 0; //Temps déja écoulé (à déduire dans countdown)
+let i = true;
 
 console.clear();
 figlet.text('sWorkTimer', {
@@ -24,20 +26,20 @@ figlet.text('sWorkTimer', {
         console.log(col.red('Erreur au lancement...'));
         return;
     }
-    console.log(gradient('white', 'cyan')(data) + col.blue('\n      Simplifier le décompte des heures de travail\n\t        Un outil de @JeyyJeyy'));
+    console.log(gradient('#CCFFFF', '#0AADFF')(data) + col.cyan('\n      Simplifier le décompte des heures de travail\n\t        Un outil de @JeyyJeyy\n\n'));
 });
-await delay(500);
-myTimer.start()
+await delay(100);
+myTimer.start();
+let num = (((myTimer.time()/1000) + mod) * 100) / (countdownn + mod);
+let txt = 'Temps écoulé: ' + convertHMS((myTimer.time()/1000) + mod) + '  Temps restant: ' + convertHMS(countdownn - (myTimer.time()/1000)) + '    [' + Math.round(num * 100) / 100 + '%]   \n';
+printProgress(txt);
 redemarrer();
 looop();
 
 function redemarrer() {
-    inp.question('Commande >> ', name => {
+    inp.question('\x1b[36msCMD >> \x1b[37m', name => {
         let args = name.toLowerCase().split(' ');
         switch (args[0]) {
-            case 'time':
-                timee();
-                break;
             case 'help':
                 help();
                 break;
@@ -51,20 +53,20 @@ function redemarrer() {
     });
 }
 
-function timee() {
-    console.log('Temps écoulé: ' + convertHMS(myTimer.time() / 1000) + ' Temps restant: ' + convertHMS(countdownn - myTimer.time()));
-    redemarrer();
-}
-
 function help() {
-    console.log('Commandes disponibles:\n-`help`: Affiche toute les commandes\n-`pause`: Pause/Dépause le minuteur\n-`end`: Arrête le minuteur\n')
+    console.log('Commandes disponibles:\n-`help`: Affiche toute les commandes\n-`pause`: Pause/Dépause le minuteur\n-`end`: Arrête le minuteur\n\n')
     redemarrer();
 }
 
 function pause() {
-    console.log('Temps avant pause: ' + convertHMS(myTimer.time() / 1000));
+    console.log('Temps avant pause: ' + convertHMS((myTimer.time()/1000) + mod));
     myTimer.toggle();
     redemarrer();
+    if(i == true){
+        i = false;
+    }else{
+        i = true;
+    }
 }
 
 function end() {
@@ -92,12 +94,26 @@ function looop() {
     setTimeout(function () {
         let timed = myTimer.time() / 1000;
         if (countdownn - timed <= 1) {
+	    readline.moveCursor(process.stdout, -12, 0);
             console.log(col.redBright('Temps de travail écoulé pour aujourd\'hui, bonne soirée !'))
             delay(300);
             end();
             delay(300);
             process.exit();
-        }
+        }else if(i == true){
+	    num = (((myTimer.time()/1000) + mod) * 100) / (countdownn + mod);
+	    txt = 'Temps écoulé: ' + convertHMS((myTimer.time()/1000) + mod) + '  Temps restant: ' + convertHMS(countdownn - (myTimer.time()/1000)) + '    [' + Math.round(num * 100) / 100
+ + '%]   \n';
+	    printProgress(txt);
+	    redemarrer();
+	}
         looop()
-    }, 10000);
+    }, 5000);
 };
+
+function printProgress(progress){
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    readline.moveCursor(process.stdout, 0, -1);
+    process.stdout.write(progress);
+}
